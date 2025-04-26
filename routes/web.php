07dc1d27b4d9a5b25d42ce983675c2fe03757c27
bly-hirata <m.hirata@bly.co.jp>
2025-04-27 01:10:28 +0900

    ビジュアルフィードバック機能プロトタイプ作成

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,4 @@ Route::get('/', function () {
 });
 Route::resource('project', 'ProjectController');
 Route::resource('sitemap', 'SiteMapController');
-Route::get('/sitemap/check/{key}', 'SiteMapController@check')->name('sitemap.check');
-Route::get('/sitemap/output', 'SiteMapController@output')->name('sitemap.output');
-Route::post('/sitemap/add', 'SiteMapController@add')->name('sitemap.add');
+Route::resource('visualfeedback', 'VisualFeedbackController');
